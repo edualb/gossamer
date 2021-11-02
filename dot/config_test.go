178082492dd6a4d6ece6a667b5fa0165173ebbdf
Edward Mack <ed@edwardmack.com>
2021-11-02 16:33:44 -0400

    move tests

--- dot/config_test.go
+++ dot/config_test.go
@@ -2,30 +2,3 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 package dot
-
-import (
-	"testing"
-
-	"github.com/ChainSafe/gossamer/lib/utils"
-
-	"github.com/stretchr/testify/require"
-)
-
-// TestExportConfig tests exporting a toml configuration file
-func TestExportConfig(t *testing.T) {
-	cfg, cfgFile := NewTestConfigWithFile(t)
-	require.NotNil(t, cfg)
-
-	genFile := NewTestGenesisRawFile(t, cfg)
-	require.NotNil(t, genFile)
-
-	defer utils.RemoveTestDir(t)
-
-	cfg.Init.Genesis = genFile.Name()
-
-	err := InitNode(cfg)
-	require.Nil(t, err)
-
-	file := ExportConfig(cfg, cfgFile.Name())
-	require.NotNil(t, file)
-}
